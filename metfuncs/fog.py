210bbff470a99fb660b1647f2b64b95826d41510
crouchr <richard.crouch100@gmail.com>
2021-02-21 23:01:13 +0000

    Added fog_algo_2 and unit tests

--- metfuncs/fog.py
+++ metfuncs/fog.py
@@ -1,13 +1,20 @@
 # algorithms for predicting fog
 # find the range that contains all three values and it must be less than a threshold I can adjust
-def fog_algo_1(temp_c, dew_point_c, wet_bulb_c, permitted_range=1.0):
+# This will work with data calculated with an AWS
+def fog_algo_1(temp_c, dew_point_c, wet_bulb_c, wind_knots_2m, permitted_range=1.0):
     """
 
     :param temp_c:
     :param dew_point_c:
     :param wet_bulb_c:
+    :param wind_knots_2m: Wind speed at 2m high (not 10m)
     :return:
     """
+
+    # fog won't form if wind speed is too high
+    if wind_knots_2m >= 2.0:
+        return False
+
     temps = [temp_c, dew_point_c, wet_bulb_c]
 
     max_temp = max(temps)
@@ -19,3 +26,24 @@ def fog_algo_1(temp_c, dew_point_c, wet_bulb_c, permitted_range=1.0):
         return True
     else:
         return False
+
+
+# This will work with data calculated with an AWS
+def fog_algo_2(temp_c, dew_point_c, wind_knots_2m):
+    """
+
+    :param temp_c:
+    :param dew_point_c:
+    :param wind_knots_2m:
+    :return:
+    """
+
+    # fog won't form if wind speed is too high
+    if wind_knots_2m >= 2.0:
+        return False
+
+    if abs(temp_c - dew_point_c) < 2.5 :
+        return True
+    else:
+        return False
+
--- metfuncs/tests/test_fog.py
+++ metfuncs/tests/test_fog.py
@@ -4,15 +4,36 @@ import fog
 
 
 @pytest.mark.parametrize(
-    "temp_c, dew_point_c, wet_bulb_c, expected",
+    "temp_c, dew_point_c, wet_bulb_c, wind_knots_2m, expected",
     [
-        (1, 1, 1, True),
-        (-1, -1, -1, True),
-        (-1, -1, -3, False),
-        (10, 20, 30, False),
+        (1, 1, 1, 1.0, True),
+        (-1, -1, -1, 1.0, True),
+        (-1, -1, -1, 2.0, False),
+        (-1, -1, -3, 1.0, False),
+        (10, 20, 30, 1.0, False),
     ]
 )
-def test_fog_1(temp_c, dew_point_c, wet_bulb_c, expected):
-    is_fog = fog.fog_algo_1(temp_c, dew_point_c, wet_bulb_c)
+def test_fog_1(temp_c, dew_point_c, wet_bulb_c, wind_knots_2m, expected):
+    is_fog = fog.fog_algo_1(temp_c, dew_point_c, wet_bulb_c, wind_knots_2m)
+
+    assert is_fog == expected
+
+
+@pytest.mark.parametrize(
+    "temp_c, dew_point_c, wind_knots_2m, expected",
+    [
+        (1, 1, 1.0, True),
+        (1, 1, 3.0, False),
+        (-1, -1, 1.0, True),
+        (-1, -1, 2.0, False),
+        (-1, -1, 1.0, True),
+        (-1, 1.4, 1.0, True),
+        (-1, 1.5, 1.0, False),
+        (-1, 1.7, 1.0, False),
+        (10, 20, 1.0, False),
+    ]
+)
+def test_fog_2(temp_c, dew_point_c, wind_knots_2m, expected):
+    is_fog = fog.fog_algo_2(temp_c, dew_point_c, wind_knots_2m)
 
     assert is_fog == expected
