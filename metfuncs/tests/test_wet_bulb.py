64cc59b754b376c5b37477329a2cf004582b6647
crouchr <richard.crouch100@gmail.com>
2021-02-22 08:37:56 +0000

    Updated wet_bulb unit test

--- metfuncs/tests/test_wet_bulb.py
+++ metfuncs/tests/test_wet_bulb.py
@@ -6,7 +6,7 @@ import wet_bulb
 @pytest.mark.parametrize(
     "temp_c, pressure_mbar, dew_point_c, expected",
     [
-        (31, 1013.25, 30, 30.7),
+        (31.0, 1013.25, 30.0, 30.7),
     ]
 )
 def test_wet_bulb(temp_c, pressure_mbar, dew_point_c, expected):
