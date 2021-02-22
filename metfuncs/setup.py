f57c9f3529ee8c217d950d1c5b71a17f19149076
crouchr <richard.crouch100@gmail.com>
2021-02-22 00:04:01 +0000

    Bumped version

--- metfuncs/setup.py
+++ metfuncs/setup.py
@@ -3,12 +3,12 @@
 
 from setuptools import setup, find_packages
 
-setup(version='1.0.10',
+setup(version='1.0.11',
       description='Functions for handling meteorological data',
       author='Richard Crouch',
       author_email='richard.crouch100@gmail.com',
       license='MIT',
       include_package_data=True,
-      py_modules=['metfuncs', 'windrose', 'solar_funcs', 'wind_calibration'],
+      py_modules=['metfuncs', 'windrose', 'solar_funcs', 'wind_calibration', 'feels_like', 'wet_bulb', 'fog', 'synopsis'],
       zip_safe=False
       )
