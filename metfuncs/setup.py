e94aeab3352733461926ff61738163bccdb47fb9
crouchr <richard.crouch100@gmail.com>
2021-02-22 00:19:04 +0000

    Bumped version

--- metfuncs/setup.py
+++ metfuncs/setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-setup(version='1.0.11',
+setup(version='1.0.12',
       description='Functions for handling meteorological data',
       author='Richard Crouch',
       author_email='richard.crouch100@gmail.com',
