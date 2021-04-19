e8714ca26222797e96a3ad65addb1ee0cfec4ea0
crouchr <richard.crouch100@gmail.com>
2021-04-19 10:28:49 +0100

    Corrected mean_sea_level_pressure

--- metfuncs/mean_sea_level_pressure.py
+++ metfuncs/mean_sea_level_pressure.py
@@ -2,7 +2,9 @@
 # https://stackoverflow.com/questions/50508262/using-look-up-tables-in-python
 
 # See Table 7.2 Burt book
-# Stockcross 90m
+# Stockcross 90m - not confirmed
+# Currently using 50m to see if it lines up better with OpenWeatherAPI
+# TODO : Could make more even accurate by adding interpolation
 def msl_k_factor(height, temp_c):
     """
     See Burt Table 7.2 p.177
@@ -14,6 +16,7 @@ def msl_k_factor(height, temp_c):
     press_map = {
         10: [1.3, 1.3, 1.2, 1.2, 1.1],
         20: [2.6, 2.5, 2.4, 2.3, 2.3],
+        50: [6.5, 6.3, 6.0, 5.8, 5.6],
         90: [11.7, 11.3, 10.9, 10.5, 10.2]
     }
 
