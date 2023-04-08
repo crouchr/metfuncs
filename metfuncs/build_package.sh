afc40f85ac55b83ae30c060ac1979881bd532493
Richard Crouch <richard.crouch@protonmail.com>
2023-04-08 07:24:56 +0100

    Replaced sea level pressure calculation with a more reliable one

--- metfuncs/build_package.sh
+++ metfuncs/build_package.sh
@@ -1,7 +1,9 @@
 # When performing a release:
 # - increment version number in setup.py
 # - run this script
-# - reboot the web-server Vagrant machine
+# - reboot the web-server Vagrant machine or vagrant up --provision
+# - check package at http://192.168.1.102/python-packages/metfuncs/
+# - update the calling project's Pipfile
 
 pipenv run python setup.py bdist_wheel
 pipenv run python setup.py sdist
