e10ebbe392f865aa020199a404fe065d43cbb5f3
Richard Crouch <richard.crouch@protonmail.com>
2023-04-08 07:21:19 +0100

    Replaced sea level pressure calculation with a more reliable one

--- metfuncs/setup.py
+++ metfuncs/setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-setup(version='1.0.30',
+setup(version='1.0.31',
       description='Functions for handling meteorological data',
       author='Richard Crouch',
       author_email='richard.crouch100@gmail.com',
